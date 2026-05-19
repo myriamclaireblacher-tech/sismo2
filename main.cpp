43eaf184c71d6274ca9868b5305ee68dc0ea4255
myriamclaireblacher-tech <myriamclaire.blacher@gmail.com>
2026-05-19 17:26:18 +0200

    librairies

--- main.cpp
+++ main.cpp
@@ -1,12 +1,22 @@
-#include <omp.h>
+//librairies générales
 #include <chrono>
-#include <cvode/cvode.h>
-#include <nvector/nvector_serial.h>
 #include <iostream> 
 #include <list>
-using namespace std;
+#include <vector>
+
+//Librairies CVCode 
+#include <cvode/cvode.h>            /* prototypes for CVODE fcts., consts.  */
+#include <nvector/nvector_serial.h> /* access to serial N_Vector            */
+#include <stdio.h>
+#include <sunlinsol/sunlinsol_dense.h> /* access to dense SUNLinearSolver      */
+#include <sunmatrix/sunmatrix_dense.h> /* access to dense SUNMatrix            */
+
+//Libraries HPC
+#include <omp.h>
+
+
 
 
 int main(){
-    cout<<"hello";
+    std::cout<<"hello";
 }
