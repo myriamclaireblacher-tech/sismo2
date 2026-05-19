fec11b89141b0c0d0b5607127fe82c412eb525b5
myriamclaireblacher-tech <myriamclaire.blacher@gmail.com>
2026-05-19 12:06:59 +0200

    plus de librairies + readme

--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
-include <omp.h>
+#include <omp.h>
 #include <chrono>
 #include <cvode/cvode.h>
 #include <nvector/nvector_serial.h>
+#include <iostream> 
+#include <list>
+using namespace std;
 
 
 int main(){
-    cout<<"hello";
+    std::cout<<"hello";
 }
