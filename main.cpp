a4cd53f97c092c9b74e244c8187390f66bc670d7
myriamclaireblacher-tech <myriamclaire.blacher@gmail.com>
2026-05-20 09:25:52 +0200

    librairies

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
 //librairies générales
-#include <chrono>
-#include <iostream> 
 #include <list>
 #include <vector>
+#include <iostream> 
+#include <iomanip>
 
 //Librairies CVCode 
 #include <cvode/cvode.h>            /* prototypes for CVODE fcts., consts.  */
@@ -12,6 +12,7 @@
 #include <sunmatrix/sunmatrix_dense.h> /* access to dense SUNMatrix            */
 
 //Libraries HPC
+#include <chrono>
 #include <omp.h>
 
 
