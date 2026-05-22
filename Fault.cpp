189516a395c321bdc087a0568ab7340a72ecc510
myriamclaireblacher-tech <myriamclaire.blacher@gmail.com>
2026-05-22 10:59:56 +0200

    Fault.cpp with mistakes

---
+++ Fault.cpp
@@ -0,0 +1,127 @@
+#include <cstdio>
+#include "ODE.hpp"
+
+//additional general librairies
+
+#include <cmath>
+#include <stdio.h>
+#include <fstream> 
+
+//additional CVCode  librairies
+#include <sunlinsol/sunlinsol_dense.h> /* access to dense SUNLinearSolver      */
+#include <sunmatrix/sunmatrix_dense.h> /* access to dense SUNMatrix            */
+
+Fault::Fault{
+                int retval = SUNContext_Create(SUN_COMM_NULL, &this->sunctx);
+                if (check_retval(&retval, "SUNContext_Create", 1)){ throw std::runtime_error("\nError : cannot create SUNContext.");}
+                this->y=N_VNew_Serial(NEQ, this->sunctx);
+                if (check_retval((void*)y, "N_VNew_Serial", 0)) {throw std::runtime_error("\nError : cannot create N_vector y."); }
+                this->abstol = N_VNew_Serial(NEQ, this->sunctx) ;
+                if (check_retval((void*)abstol, "N_VNew_Serial", 0)) {throw std::runtime_error("\nError : cannot create N_vector abstol."); }
+                sunrealtype* y_dot = N_VGetArrayPointer(this->abstol);
+                y_dot[0]=ATOL1 ; //error on V
+                y_dot[1]=ATOL2 ; //error on theta
+                this->cvode_mem = CVodeCreate(CV_BDF, this->sunctx);
+                if (check_retval((void*)cvode_mem, "CVodeCreate", 0)) {throw std::runtime_error("\nError : cannot create cvode_mem."); }
+                retval = CVodeInit(cvode_mem, f, SUN_RCONST(0.0), this->y);
+                if (check_retval(&retval, "CVodeInit", 1)) { throw std::runtime_error("\nError : cannot init cvode_mem."); }
+                retval = CVodeSVtolerances(cvode_mem, RTOL, abstol);
+                if (check_retval(&retval, "CVodeSVtolerances", 1)) {  throw std::runtime_error("\nError : issues with tolerance."); }
+                this->A = SUNDenseMatrix(NEQ, NEQ, this->sunctx);
+                if (check_retval((void*)A, "SUNDenseMatrix", 0)) {throw std::runtime_error("\nError : cannot create SunMatrix A."); };
+                this->LS = SUNLinSol_Dense(this->y, this->A, this->sunctx);
+                if (check_retval((void*)LS, "SUNLinSol_Dense", 0)) { throw std::runtime_error("\nError : cannot create SUNLinsol LS."); }
+                retval = CVodeSetLinearSolver(this->cvode_mem, this->LS, this->A);
+                if (check_retval(&retval, "CVodeSetLinearSolver", 1)) { throw std::runtime_error("\nError : cannot attach Linear Solver."); }
+                retval = CVodeSetJacFn(this->cvode_mem, Jac);
+                if (check_retval(&retval, "CVodeSetJacFn", 1)) { throw std::runtime_error("\nError : cannot attach Jacobian."); }
+        }
+
+~Fault::Fault(){    
+            if (this->LS)        SUNLinSolFree(this->LS);           /* Free the linear solver memory */
+            if (this->A)         SUNMatDestroy(this->A);            /* Free the matrix memory */
+            if (this->cvode_mem) CVodeFree(&this->cvode_mem);       /* Free CVODE memory */
+            if (this->abstol)    N_VDestroy(this->abstol);          /* Free abstol vector */
+            if (this->y)         N_VDestroy(this->y);               /* Free y vector */
+            if (this->sunctx)    SUNContext_Free(&this->sunctx);    /* Free the SUNDIALS context */}
+
+std::vector<sunrealtype> V_list_create(const std::vector<sunrealtype>& t_list){
+    std::vector<sunrealtype> V_list;
+    V_list.reserve(t_list.size());
+    return V_list;
+}
+
+int ODE_solver(const std::vector<sunrealtype>& t_list, std::vector<sunrealtype>& V_list, const Param& fault_param){
+    const sunrealtype V1 = fault_param->V0_ * std::exp(fault_param->Dtau_asigma) ; //initial slip rate 
+    const sunrealtype theta1 = 1.0/(fault_param->D_c_inv * fault_param->V0_ ); //initial theta
+    v_list[0] = V1 ;
+
+    sunrealtype* y_data = N_VGetArrayPointer(this->y); /////////////////////////////////
+    y_data[0] = V1 ;   //slip rate component for fixed time t=1
+    y_data[1] = theta1 ;  //state variable component for fixed time t=0
+
+    // parameters of the fault
+    retval = CVodeSetUserData(this->cvode_mem, const_cast<Param*>(&fault_param)); ///////////////
+    if (check_retval(&retval, "CVodeSetUserData", 1)) { return 1; }
+
+    sunrealtype t;
+
+    for (int i=1; i<t_list.size(); ++i){
+        retval = CVode(cvode_mem, t_list[i], y, &t, CV_NORMAL);
+        if (check_retval(&retval, "CVode", 1)) { break; }
+        if (retval == CV_SUCCESS) V_list[i]=y_data[0];
+    }
+    /* Print final statistics to the screen */
+    if (rapport_EDO)
+    {
+        printf("\nFinal Statistics:\n");
+        retval = CVodePrintAllStats(cvode_mem, stdout, SUN_OUTPUTFORMAT_TABLE);
+    }
+
+    return (retval);
+}
+
+int ODE_solver(std::ofstream& file, const std::vector<sunrealtype>& t_list,  Param& fault_param){
+
+    const sunrealtype V1 = fault_param->V0_ * std::exp(fault_param->Dtau_asigma) ; //initial slip rate 
+    const sunrealtype theta1 = 1.0/(fault_param->D_c_inv * fault_param->V0_ ); //initial theta
+
+
+    sunrealtype* y_data = N_VGetArrayPointer(this->y); /////////////////////////////////
+    y_data[0] = V1 ;   //slip rate component for fixed time t=1
+    y_data[1] = theta1 ;  //state variable component for fixed time t=0
+
+    // parameters of the fault
+    retval = CVodeSetUserData(this->cvode_mem, const_cast<Param*>(&fault_param)); ///////////////
+    if (check_retval(&retval, "CVodeSetUserData", 1)) { return 1; }
+
+    sunrealtype t;
+
+    std::vector<SolValues> history_res;
+    history_res.reserve(t_list.size());
+    for (int i=1; i<t_list.size(); ++i){
+        retval = CVode(this->cvode_mem, t_list[i], this->y, &t, CV_NORMAL);
+        if (check_retval(&retval, "CVode", 1)) { break; }
+        if (retval == CV_SUCCESS) history_res.push_back({t,y_data[0],y_data[1]});
+    }
+    /* Print final statistics to the screen */
+    if (rapport_EDO)
+    {
+        printf("\nFinal Statistics:\n");
+        retval = CVodePrintAllStats(this->cvode_mem, stdout, SUN_OUTPUTFORMAT_TABLE);
+    }
+
+
+    /* Write data out to the CSV file */
+    if (file.is_open()) {
+        file << "Time,V,Theta\n"; // CSV Header
+        for (const auto& sol : history_res) {
+            file << sol.t << "," << sol.V << "," << sol.theta << "\n";
+        }
+    } else {
+        printf("\nWarning: Output file stream is not open. Data not saved.\n");
+    }
+    return (retval);
+
+}
+
--- Fault.hpp
+++ Fault.hpp
@@ -30,40 +30,9 @@ class Fault{
         SUNLinearSolver LS;
         void* cvode_mem ; 
     public:
-        Fault(){
-                int retval = SUNContext_Create(SUN_COMM_NULL, &this->sunctx);
-                if (check_retval(&retval, "SUNContext_Create", 1)){ throw std::runtime_error("\nError : cannot create SUNContext.");}
-                this->y=N_VNew_Serial(NEQ, this->sunctx);
-                if (check_retval((void*)y, "N_VNew_Serial", 0)) {throw std::runtime_error("\nError : cannot create N_vector y."); }
-                this->abstol = N_VNew_Serial(NEQ, this->sunctx) ;
-                if (check_retval((void*)abstol, "N_VNew_Serial", 0)) {throw std::runtime_error("\nError : cannot create N_vector abstol."); }
-                sunrealtype* y_dot = N_VGetArrayPointer(this->abstol);
-                y_dot[0]=ATOL1 ; //error on V
-                y_dot[1]=ATOL2 ; //error on theta
-                this->cvode_mem = CVodeCreate(CV_BDF, this->sunctx);
-                if (check_retval((void*)cvode_mem, "CVodeCreate", 0)) {throw std::runtime_error("\nError : cannot create cvode_mem."); }
-                retval = CVodeInit(cvode_mem, f, SUN_RCONST(0.0), this->y);
-                if (check_retval(&retval, "CVodeInit", 1)) { throw std::runtime_error("\nError : cannot init cvode_mem."); }
-                retval = CVodeSVtolerances(cvode_mem, RTOL, abstol);
-                if (check_retval(&retval, "CVodeSVtolerances", 1)) {  throw std::runtime_error("\nError : issues with tolerance."); }
-                this->A = SUNDenseMatrix(NEQ, NEQ, this->sunctx);
-                if (check_retval((void*)A, "SUNDenseMatrix", 0)) {throw std::runtime_error("\nError : cannot create SunMatrix A."); };
-                this->LS = SUNLinSol_Dense(this->y, this->A, this->sunctx);
-                if (check_retval((void*)LS, "SUNLinSol_Dense", 0)) { throw std::runtime_error("\nError : cannot create SUNLinsol LS."); }
-                retval = CVodeSetLinearSolver(this->cvode_mem, this->LS, this->A);
-                if (check_retval(&retval, "CVodeSetLinearSolver", 1)) { throw std::runtime_error("\nError : cannot attach Linear Solver."); }
-                retval = CVodeSetJacFn(this->cvode_mem, Jac);
-                if (check_retval(&retval, "CVodeSetJacFn", 1)) { throw std::runtime_error("\nError : cannot attach Jacobian."); }
-
-        };
+        Fault();
     
-        ~Fault(){    
-            if (this->LS)        SUNLinSolFree(this->LS);           /* Free the linear solver memory */
-            if (this->A)         SUNMatDestroy(this->A);            /* Free the matrix memory */
-            if (this->cvode_mem) CVodeFree(&this->cvode_mem);       /* Free CVODE memory */
-            if (this->abstol)    N_VDestroy(this->abstol);          /* Free abstol vector */
-            if (this->y)         N_VDestroy(this->y);               /* Free y vector */
-            if (this->sunctx)    SUNContext_Free(&this->sunctx);    /* Free the SUNDIALS context */};
+        ~Fault();
         
         //create 
         std::vector<sunrealtype> V_list_create(const std::vector<sunrealtype>& t_list);
