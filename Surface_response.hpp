6cc2cc6e7b326307f14866ca9e56239841b6f19b
myriamclaireblacher-tech <myriamclaire.blacher@gmail.com>
2026-05-27 08:58:19 +0200

    Surface response peut être fini

--- Surface_response.hpp
+++ Surface_response.hpp
@@ -2,19 +2,38 @@
 #define SURFACE_RESPONSE
 
 #include "ODE.hpp"
+#include <omp.h>
 
+//one for each thread
+struct ThreadWorkspace {
+    Fault fault_solver;
+    std::vector<sunrealtype> V_list;
+};
 
-int signal_surface(std::vector<Param>*pP, std::vector<sunrealtype>& t_list,  std::vector<sunrealtype>& V_list,
-                        Eigen::Matrix<double,,NSubFaults, 3*Nstations>& G, 
+int signal_surface(std::vector<Param>*pP, std::vector<sunrealtype>& t_list,  
+                        std::vector<ThreadWorkspace>& workspaces,
+                        Eigen::Matrix<double,3*Nstations,NSubFaults>& G, 
                         Eigen::Matrix<double, 3*Nstations, Eigen::Dynamic>& RES_matrix, 
                         Eigen::Matrix <double, NSubFaults, Eigen::Dynamic, Eigen::RowMajor> & storage_matrix){
-    for (size_t i = 0; i < NSubFaults; i++)
+
+    
+
+
+    #pragma omp parallel 
     {
-        ODE_solver(t_list,V_list, (*pP)[i]);
-        compute_slip( storage_matrix.row(i) , t_list , V_list);
-    }
+        //get thread data
+        int tid = omp_get_thread_num(); 
+        ThreadWorkspace& ws = workspaces[tid];
+        #pragma omp for
+            for (size_t i = 0; i < NSubFaults; i++)
+            {   ws.fault_solver.ODE_solver(t_list, ws.V_list, (*pP)[i]);
+                compute_slip( storage_matrix.row(i) , t_list , ws.V_list);}
+
+            
+            
+     }
     RES_matrix.noalias() = G * storage_matrix ;
     return 0;
+        
     }
-
 #endif
