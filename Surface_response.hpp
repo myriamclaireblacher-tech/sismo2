a7e6c5c8db8d81ce98f06c9550a6a1c2dcb8b3c0
myriamclaireblacher-tech <myriamclaire.blacher@gmail.com>
2026-06-04 14:32:52 +0200

    compute llk

--- Surface_response.hpp
+++ Surface_response.hpp
@@ -6,6 +6,7 @@
 
 
 
+/*
 int surface_response(std::vector<Param>& pP, std::vector<sunrealtype>& t_list,  
                         std::vector<Fault>& fault_per_thread,
                         Eigen::Matrix<double,3*Nstations,NSubFaults>& G, 
@@ -28,4 +29,21 @@ int surface_response(std::vector<Param>& pP, std::vector<sunrealtype>& t_list,
         
    return 0;       
 }
+   */
+
+int surface_response(std::vector<Param>& pP, std::vector<sunrealtype>& t_list,  
+                        std::vector<Fault>& fault_per_thread,
+                        Eigen::Matrix<double,3*Nstations,NSubFaults>& G, 
+                        Eigen::Matrix<double, 3*Nstations, Eigen::Dynamic>& RES_matrix, 
+                        Eigen::Matrix <double, NSubFaults, Eigen::Dynamic, Eigen::RowMajor> & storage_matrix){
+        
+    for (size_t i = 0; i < NSubFaults; i++)
+    {   int tid = omp_get_thread_num(); 
+        fault_per_thread[tid].ODE_solver(t_list, storage_matrix.row(i), pP[i]);}
+            
+    RES_matrix.noalias() = G * storage_matrix ;
+        
+   return 0;       
+}
+
 #endif
--- inversion.hpp
+++ inversion.hpp
@@ -4,6 +4,8 @@
 #include "Surface_response.hpp"
 #include <random>
 #include <cmath>
+#include <string>
+#include <fstream>
 
 struct PT_param {
     double k_a_sigma_inf;           // k/ (a sigma) lower bound
@@ -23,37 +25,54 @@ struct PT_param {
     D_c_inv_sup (D_c_inv_sup_ent), Dtau_asigma_inf(Dtau_asigma_inf_ent), Dtau_asigma_sup (Dtau_asigma_sup_ent), T_max(T_max_ent),
     nchains (nchains_ent), ncold(ncold_ent)
     {}   
-}
-
+};
 
 struct ThreadWorkspace {
     std::vector<Param> pP;
-    pP.resize(NSubFaults);
     std::vector<Fault> Faults;
-    Faults.resize(NSubFaults);
     Eigen::MatrixXd RES_matrix;
     Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> storage_matrix;
 
-    ThreadWorkspace(int n_stations, int t_list_size) {
+    ThreadWorkspace(int t_list_size) {
         pP.reserve(NSubFaults);
-    
-        RES_matrix.resize(3 * n_stations, t_list_size);
+        Faults.resize(NSubFaults);
+        RES_matrix.resize(3 * Nstations, t_list_size);
         storage_matrix.resize(NSubFaults, t_list_size);
     }
-
     ThreadWorkspace() = default; 
-}
+};
 
-int compute_llk(const Param *P){
+struct ColdChainSaver {
+    std::ofstream model_file;
+    std::ofstream llk_file;
+
+    // One file for each cold chain
+    ColdChainSaver(int cold_idx) {
+        model_file.open("models_cold_" + std::to_string(cold_idx) + ".txt");
+        llk_file.open("llk_cold_" + std::to_string(cold_idx) + ".txt");   
+    }
+
+    void save_step(const Param& m, double energy) {
+        //save model
+        model_file << m.k_a_sigma << " " << m.b_a << " " << m.D_c_inv << " " << m.Dtau_asigma << "\n";
+        //save log-likelyhood
+        llk_file << energy << "\n";
+    }
+};
+
+double compute_llk(const std::vector<sunrealtype>& t_list, const  Eigen::Matrix<double, 3*Nstations, Eigen::Dynamic>& data,
+                    Eigen::Matrix<double,3*Nstations,NSubFaults>& G, ThreadWorkspace& work,double T){
+
+    int SR = surface_response(work.pP, t_list, work.Faults, G, work.RES_matrix, work.storage_matrix);
+    if (SR!=0) return -std::numeric_limits<double>::infinity();
+    double llk = - (data - work.RES_matrix).array().square().sum()/T;
+    return llk;
+};
 
 
-    
-}
 
 
-
-
-int parallel_tempering(const int maxint, const PT_param PT, std::vector<double> data, std::vector<double> t_list,  int seed=42, double sigma=0.05){
+int parallel_tempering(const int maxint, const PT_param PT, Eigen::Matrix<double, 3*Nstations, Eigen::Dynamic>& data, std::vector<double> t_list,  int seed=42, double sigma=0.05){
 
     //check t_list size, data size
 
@@ -65,8 +84,12 @@ int parallel_tempering(const int maxint, const PT_param PT, std::vector<double> 
     for (int t = 0; t < NCPU; ++t) {
         workspaces.emplace_back(NSubFaults, Nstations, t_list.size());
     }
+    //storage of the models of the cold chains
+    std::vector<ColdChainSaver> savers;
+    for (int i = 0; i < PT.ncold; ++i) {
+        savers.emplace_back(i);
+    }
     
-
     //Temperatures of the chains
     std::mt19937 gen(seed); //generate random
     std::vector<double> T(nchains); 
@@ -89,13 +112,18 @@ int parallel_tempering(const int maxint, const PT_param PT, std::vector<double> 
         P.push_back(Param(p1, p2, p3, p4));
     }
 
+    //save initial models in files for cold chains
 
+
+
+    //Parallel tempering
     for (int i=0; i<maxint ; i++){
 
         if (it % (itmax / 10) == 0) {
             std::cout << std::setw(3) << (100 * it / itmax) << "% done" << std::endl;
         }
 
+        //add CPU
         for (int ichain=0; ichain<PT.nchains ; ichain++){
             Param Mnew = P[ichain];
             Mnew.k_a_sigma   += unif_dist(gen) * (PT.k_a_sigma_sup   - PT.k_a_sigma_inf) * sigma;
